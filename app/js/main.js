baf39d88cdc5c700743d02b837527768c05edad5
Joshua Melville <joshmelville@gmail.com>
2015-05-15 01:33:09 +0100

    animation protection

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 /* exported App, timedStages */
-/* global $, Hammer, video, snapshot,localMediaStream:true,hdConstraints,ctx, canvas, slideTimer, Codebird */
+/* global global, require, $, Hammer, video, snapshot,localMediaStream:true,hdConstraints,ctx, canvas, slideTimer, Codebird */
 /* jshint devel:true,unused:false */
 
 var App = function App() {
@@ -8,6 +8,7 @@ var App = function App() {
   var resetTimer;
   var overlayOpen = false;
   var opening = false;
+  var animating = false;
   var currentItem;
   var content = {
       'item-1': {
@@ -21,40 +22,48 @@ var App = function App() {
   };
 
   function tapHandler(e) {
-      if (!opening) {
-          opening = true;
-          if (!overlayOpen) {
-              $('.blackout').fadeIn();
-              console.log($(e.target).attr('class').split(' '));
-              currentItem = $(e.target).attr('class').split(' ').pop(); // This will put the last class name (item-n) in the currentItem var. Not a jQuery object though!
-              console.log('current item is:'+ currentItem);
-              $(e.target).addClass('front');
-              app.openOverlay();
+      if (!animating) {
+          if (!opening) {
+              opening = true;
+              if (!overlayOpen) {
+                  console.log($(e.target).attr('class').split(' '));
+                  currentItem = $(e.target).attr('class').split(' ').pop(); // This will put the last class name (item-n) in the currentItem var. Not a jQuery object though!
+                  console.log('current item is:'+ currentItem);
+                  app.openOverlay();
+              } else {
+                  app.closeOverlay();
+                  currentItem = null;
+              }
+
           } else {
-              app.closeOverlay();
-              currentItem = null;
+              return false;
           }
-
-      } else {
-          return false;
       }
-
   }
 
   app.init = function() {
       $('.cabinet-item').hammer().bind('tap', tapHandler);
       $('.back').hammer().bind('tap', app.closeOverlay);
+
+      if (typeof global !== 'undefined') {
+          global.gui = require('nw.gui');
+          global.gui.Window.get().enterFullscreen();
+      }
+
   };
 
   app.openOverlay = function() {
-
-      // Ajax load the item content into the container div
-      $('.item-content').load('../items/'+currentItem+'.html', function() {
-          $('.swiper-container').hide();
-          // Once we are finished loading, set up the animation stuff
-          setTimeout(function () {
-              $('.'+currentItem).toggleClass('active');
-              $('.overlay').toggleClass('active');
+      if (!animating) {
+          $('.blackout').fadeIn();
+          $('.'+currentItem).addClass('front');
+          animating = true;
+          console.log('loading content.');
+          // Ajax load the item content into the container div
+          $('.item-content').load('../items/'+currentItem+'.html', function() {
+              $('.swiper-container').hide();
+              // Once we are finished loading, set up the animation stuff
+              $('.'+currentItem).addClass('active');
+              $('.overlay').addClass('active');
               opening = false;
               overlayOpen = true;
               setTimeout(function () {
@@ -65,24 +74,35 @@ var App = function App() {
                           $(value).addClass('para-shown');
                       }, (index+2)*250);
                   });
+                  setTimeout(function () {
+                      animating = false;
+                  }, ($('.content-row > *').length+2)*250);
+
               }, 1000);
-          }, 0);
-      });
+          });
+      }
+
   };
 
   app.closeOverlay = function() {
-      $('.swiper-container').hide();
-      $('.'+currentItem).fadeIn();
-      $('.'+currentItem).toggleClass('active');
-      $('.overlay').toggleClass('active');
-      $('.content-row > *').removeClass('para-shown');
-      setTimeout(function () {
-          opening = false;
-          overlayOpen = false;
-          $.when($('.blackout').fadeOut()).done(function(){
-              $('.cabinet-item').removeClass('front');
-          });
-      }, 1000);
+      if(!animating) {
+          animating = true;
+          $('.swiper-container').hide();
+          $('.'+currentItem).fadeIn();
+          $('.'+currentItem).removeClass('active');
+          $('.overlay').removeClass('active');
+          $('.content-row > *').removeClass('para-shown');
+          setTimeout(function () {
+              opening = false;
+              overlayOpen = false;
+              $.when($('.blackout').fadeOut()).done(function(){
+                  $('.cabinet-item').removeClass('front');
+                  $('.item-content').empty();
+                  animating = false;
+              });
+          }, 1000);
+      }
+
   };
 
   app.toggleOverlay = function() {
